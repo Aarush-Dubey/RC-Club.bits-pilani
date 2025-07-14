1cc9eec35d4fb0ca1fb14067f509c0fc9f512cc2
AARUSH DUBEY <f20241284@pilani.bits-pilani.ac.in>
2025-07-14 14:00:07 +0000

    --- title: Next.js ImageKit SDK description: Real-time image & video res

--- src/app/api/upload-auth/route.ts
+++ src/app/api/upload-auth/route.ts
@@ -5,13 +5,13 @@ export async function GET() {
     // Your application logic to authenticate the user could go here.
     // For this demo, we'll assume the user is authenticated.
 
-    if (!process.env.IMAGEKIT_PRIVATE_KEY || !process.env.IMAGEKIT_PUBLIC_KEY) {
+    if (!process.env.IMAGEKIT_PRIVATE_KEY || !process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY) {
         return Response.json({ error: "ImageKit keys are not configured." }, { status: 500 });
     }
 
     const { token, expire, signature } = getUploadAuthParams({
         privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
-        publicKey: process.env.IMAGEKIT_PUBLIC_KEY as string,
+        publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY as string,
     })
 
     return Response.json({ token, expire, signature })
