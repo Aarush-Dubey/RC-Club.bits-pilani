8b1eeccb451785d303564b9eabae071734c0f6c1
AARUSH DUBEY <f20241284@pilani.bits-pilani.ac.in>
2025-08-05 17:39:48 +0000

    Google API Key AIzaSyBXBN-Ts29NO9vtAqCOfoB5HDmHSZ0iSks Public leak #1 op

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,13 +8,13 @@ import { getAuth, initializeAuth, indexedDBLocalPersistence } from "firebase/aut
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBXBN-Ts29NO9vtAqCOfoB5HDmHSZ0iSks",
-  authDomain: "rc-manager-65085.firebaseapp.com",
-  projectId: "rc-manager-65085",
-  storageBucket: "rc-manager-65085.appspot.com",
-  messagingSenderId: "63464032218",
-  appId: "1:63464032218:web:7b8eec3a72f4badfa222e3",
-  measurementId: "G-64C6XG9QNB"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
