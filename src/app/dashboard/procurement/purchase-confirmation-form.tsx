18fd44ba195daf2f5829ad6651d54fa3fd034356
AARUSH DUBEY <f20241284@pilani.bits-pilani.ac.in>
2025-09-07 14:26:58 +0000

    Try fixing this error: `Console Error: A component is changing an uncont

--- src/app/dashboard/procurement/purchase-confirmation-form.tsx
+++ src/app/dashboard/procurement/purchase-confirmation-form.tsx
@@ -45,6 +45,7 @@ export function PurchaseConfirmationForm({ onFormSubmit, currentUser, procuremen
     resolver: zodResolver(formSchema),
     defaultValues: {
         actualCost: procurementRequest.expectedCost,
+        vendor: "",
     }
   });
 
